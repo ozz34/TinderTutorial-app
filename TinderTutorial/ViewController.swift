d2da3502b209812fd3c9d54aafac5e296fd22de8
ozz34 <halex7@yandex.ru>
2023-02-28 13:26:43 +0300

    Initial Commit

---
+++ TinderTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderTutorial
+//
+//  Created by Иван Худяков on 28.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
