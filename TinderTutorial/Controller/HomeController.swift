4c47d658b33b9743a1424dc2a294cf47b58f5ab4
ozz34 <halex7@yandex.ru>
2023-02-28 15:21:58 +0300

    2. Getting started with homepage UI

--- TinderTutorial/Controller/HomeController.swift
+++ TinderTutorial/Controller/HomeController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class HomeController: UIViewController {
-    
+    // MARK: - Properties
+    private let topStack = HomeNavigationStackView()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .white
+        view.addSubview(topStack)
+        topStack.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                        left: view.leftAnchor,
+                        right: view.rightAnchor)
     }
 }
