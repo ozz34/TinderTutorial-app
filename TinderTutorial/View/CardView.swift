efd08e4c87a993f3811421b05dc8ea8e1a362cb6
ozz34 <halex7@yandex.ru>
2023-03-01 14:05:38 +0300

    5. Finish up CardView UI

--- TinderTutorial/View/CardView.swift
+++ TinderTutorial/View/CardView.swift
@@ -9,12 +9,34 @@ import UIKit
 
 class CardView: UIView {
     // MARK: - Properties
+    private let gradientLayer = CAGradientLayer()
+    
     private let imageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
         iv.image = UIImage(named: "jane1")
         
         return iv
+    }()
+    
+    private let infoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Jane Doe",
+                                                       attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy),.foregroundColor: UIColor.white])
+        attributedText.append(NSAttributedString(string: "  20", attributes: [.font: UIFont.systemFont(ofSize: 24),.foregroundColor: UIColor.white]))
+        label.attributedText = attributedText
+        
+        return label
+    }()
+    
+    private lazy var infoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "info_icon")?.withRenderingMode(.alwaysOriginal),
+                        for: .normal)
+
+        return button
     }()
     
     // MARK: - Lifecycle
@@ -26,9 +48,39 @@ class CardView: UIView {
         
         addSubview(imageView)
         imageView.fillSuperview()
+        
+        configureGradientLayer()
+        
+        addSubview(infoLabel)
+        infoLabel.anchor(left: leftAnchor,
+                         bottom: bottomAnchor,
+                         right: rightAnchor,
+                         paddingLeft: 16,
+                         paddingBottom: 16,
+                         paddingRight: 16)
+        
+        addSubview(infoButton)
+        infoButton.setDimensions(height: 40, width: 40)
+        infoButton.centerY(inView: infoLabel)
+        infoButton.anchor(right: rightAnchor,
+                         paddingRight: 16)
+        
+       
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+       gradientLayer.frame = self.frame
+    }
+    
+    // MARK: - Helpers
+    func configureGradientLayer() {
+        gradientLayer.colors = [UIColor.clear.cgColor,
+                                UIColor.black.cgColor]
+        gradientLayer.locations = [0.5, 1.1]
+        layer.addSublayer(gradientLayer)
     }
 }
