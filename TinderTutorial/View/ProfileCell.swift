795476e6668539ffadafd30c2d87adce42edaadb
ozz34 <halex7@yandex.ru>
2023-03-12 12:18:02 +0300

    41. Profile user interface

---
+++ TinderTutorial/View/ProfileCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileCell.swift
+//  TinderTutorial
+//
+//  Created by Иван Худяков on 10.03.2023.
+//
+
+import UIKit
+
+class ProfileCell: UICollectionViewCell {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
